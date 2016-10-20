58916c87aa91870a05425395d052c1bf74cc5486
Jaymin <jayminpanchal10@gmail.com>
2016-10-20 23:17:09 +0530

    --Added swagger ui

---
+++ app/Http/Controllers/Api/DashboardController.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Reshma
+ * Date: 10/20/2016
+ * Time: 11:10 PM
+ */
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Requests;
+use App\Http\Controllers\ApiController;
+/**
+ * Class DashboardController
+ *
+ * @package App\Http\Controllers\Api
+ */
+class DashboardController extends ApiController {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     *
+     * @SWG\Get(
+     *     path="/api/dashboard",
+     *     description="Returns dashboard overview.",
+     *     operationId="api.dashboard.index",
+     *     produces={"application/json"},
+     *     tags={"dashboard"},
+     *     @SWG\Response(
+     *         response=200,
+     *         description="Dashboard overview."
+     *     ),
+     *     @SWG\Response(
+     *         response=401,
+     *         description="Unauthorized action.",
+     *     )
+     * )
+     */
+    public function index()
+    {
+        return response()->json([
+            'result'    => [
+                'statistics' => [
+                    'users' => [
+                        'name'  => 'Jaymin',
+                        'email' => 'jayminpanchal10@gmail.com'
+                    ]
+                ],
+            ],
+            'message'   => '',
+            'type'      => 'success',
+            'status'    => 0
+        ]);
+    }
+}
---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jaymin
+ * Date: 10/20/2016
+ * Time: 11:09 PM
+ */
+
+namespace App\Http\Controllers;
+
+/**
+ * Class ApiController
+ *
+ * @package App\Http\Controllers
+ *
+ * @SWG\Swagger(
+ *     basePath="",
+ *     host="laravel-swagger",
+ *     schemes={"http"},
+ *     @SWG\Info(
+ *         version="1.0",
+ *         title="Sample API",
+ *         @SWG\Contact(name="Jaymin Panchal", email="jayminpanchal10@gmail.com"),
+ *     ),
+ *     @SWG\Definition(
+ *         definition="Error",
+ *         required={"code", "message"},
+ *         @SWG\Property(
+ *             property="code",
+ *             type="integer",
+ *             format="int32"
+ *         ),
+ *         @SWG\Property(
+ *             property="message",
+ *             type="string"
+ *         )
+ *     )
+ * )
+ */
+class ApiController extends Controller
+{
+}
